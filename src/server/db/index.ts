7bdedc0ab4a2140ab503c8bb9d144b1b24e7888d
Jose Angarita <amjose09@gmail.com>
2025-07-31 18:11:33 -0500

    db schema and singlestore changes

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,19 +1,26 @@
-import { createClient, type Client } from "@libsql/client";
-import { drizzle } from "drizzle-orm/libsql";
-
+import { createPool, type Pool } from "mysql2/promise";
 import { env } from "~/env";
-import * as schema from "./schema";
 
-/**
- * Cache the database connection in development. This avoids creating a new connection on every HMR
- * update.
- */
 const globalForDb = globalThis as unknown as {
-  client: Client | undefined;
+  conn: Pool | undefined;
 };
 
-export const client =
-  globalForDb.client ?? createClient({ url: env.DATABASE_URL });
-if (env.NODE_ENV !== "production") globalForDb.client = client;
+export const conn =
+  globalForDb.conn ??
+  createPool({
+    host: env.SINGLESTORE_HOST,
+    user: env.SINGLESTORE_USER,
+    password: env.SINGLESTORE_PASSWORD,
+    database: env.SINGLESTORE_DATABASE,
+    port: Number(env.SINGLESTORE_PORT),
+    ssl: {},
+    maxIdle: 0,
+  });
 
-export const db = drizzle(client, { schema });
+if (env.NODE_ENV !== "production") {
+  globalForDb.conn = conn;
+}
+
+conn.addListener("error", (err) => {
+  console.error("Database connection error:", err);
+});
